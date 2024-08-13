da5566f62f623a92edbf433a3a48de88388387a6
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-13 20:33:51 +0600

    Path Parameters [incompleted]

--- myapi.py
+++ myapi.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "Rafiul Islam",
+        "age": 21,
+        "class": "B.Sc in CSE"
+    }
+}
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    return students[student_id]
