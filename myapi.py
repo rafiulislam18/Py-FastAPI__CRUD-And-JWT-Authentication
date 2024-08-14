7b7271e3182defe1a019352919630d148e378710
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-14 09:20:40 +0600

    correction

--- myapi.py
+++ myapi.py
@@ -15,5 +15,5 @@ def index():
     return {"name": "First Data"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
+def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
