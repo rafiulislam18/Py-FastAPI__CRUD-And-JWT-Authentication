1f94ba6f746d88878ecdc9f861ba2b805373ec86
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-15 08:00:16 +0600

    added comments

--- myapi.py
+++ myapi.py
@@ -11,17 +11,21 @@ students = {
     }
 }
 
+# Basic API
 @app.get("/")
 def index():
     return {"name": "First Data"}
 
+# Path Parameter
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
 
+# Query Parameter (+ Path Parameter)
 @app.get("/get-by-name/{student_id}")
 def get_student(*, student_id: int, name: Optional[str] = None, text : int):
     for student_id in students:
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"Data": "Not found"}
+
