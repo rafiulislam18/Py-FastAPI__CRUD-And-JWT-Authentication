172d4bdd1430b103156241e651462528d3562dea
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-15 08:10:51 +0600

    FastAPI - Request Body

--- myapi.py
+++ myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,21 +8,26 @@ students = {
     1: {
         "name": "Rafi",
         "age": 21,
-        "class": "B.Sc in CSE"
+        "year": "B.Sc in CSE"
     }
 }
 
-# Basic API
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+# FastAPI - Basic API
 @app.get("/")
 def index():
     return {"name": "First Data"}
 
-# Path Parameter
+# FastAPI - Path Parameter
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
 
-# Query Parameter (+ Path Parameter)
+# FastAPI - Query Parameter (+ Path Parameter)
 @app.get("/get-by-name/{student_id}")
 def get_student(*, student_id: int, name: Optional[str] = None, text : int):
     for student_id in students:
@@ -29,3 +35,10 @@ def get_student(*, student_id: int, name: Optional[str] = None, text : int):
             return students[student_id]
     return {"Data": "Not found"}
 
+# FastAPI - Request Body
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student exists"}
+    students[student_id] = student
+    return students[student_id]
