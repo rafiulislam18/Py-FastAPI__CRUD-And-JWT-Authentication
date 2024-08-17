283a17e6faf6edb0f6d6240d7d92e25402b1e890
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-17 10:54:19 +0600

    crud_no_db

--- crud_no_db.py
+++ crud_no_db.py
@@ -1,3 +1,5 @@
+# CRUD Operations without Database in FastAPI
+
 from fastapi import FastAPI, Path
 from typing import Optional
 from pydantic import BaseModel
@@ -16,6 +18,11 @@ class Student(BaseModel):
     name: str
     age: int
     year: str
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
 
 # FastAPI - Basic API
 @app.get("/")
@@ -43,7 +50,28 @@ def create_student(student_id: int, student: Student):
     students[student_id] = student
     return students[student_id]
 
-# FastAPI - Request Body (simple function)
-@app.post("/create-student/")
-def simple_request_body(s: Student):
-    return s
+# FastAPI - Put Method / Update Operation
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+    
+    if student.name != None:
+        students[student_id]["name"] = student.name
+
+    if student.age != None:
+        students[student_id]["age"] = student.age
+
+    if student.year != None:
+        students[student_id]["year"] = student.year
+    
+    return students[student_id]
+
+# FastAPI - Delete Method / Delete Operation
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+    
+    del students[student_id]
+    return {"Message": "Student deleted successfully"}
