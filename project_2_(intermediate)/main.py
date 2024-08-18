186721dc13e4ea87ad970fd3640260874a3dc17d
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-18 08:27:13 +0600

    Project - 2 : Pydantic Schemas

--- project_2_(intermediate)/main.py
+++ project_2_(intermediate)/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -23,6 +25,20 @@ def show(id: int):
     return {"data": id}
 
 @app.get("/blog/{id}/comments")
-def comments(id):
+def comments(id, limit=10):
     # fetch comments of blog with id = id
     return {"data": {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {"data": f"Blog is created with title as {blog.title}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
