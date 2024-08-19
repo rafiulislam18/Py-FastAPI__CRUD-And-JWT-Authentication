38d7281d34f733ee598920621f270ab9496173fc
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-19 09:36:26 +0600

    Response Model

--- project_2_(intermediate)/blog/main.py
+++ project_2_(intermediate)/blog/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
+from typing import List
 import schemas
 import models
 from database import engine, SessionLocal
@@ -41,12 +42,12 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return "updated"
 
-@app.get("/blog")
+@app.get("/blog", response_model=List[schemas.ShowBlog])
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}", status_code=200)
+@app.get("/blog/{id}", status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
--- project_2_(intermediate)/blog/schemas.py
+++ project_2_(intermediate)/blog/schemas.py
@@ -5,3 +5,10 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBlog(Blog):
+    title: str
+    body: str
+
+    class Config():
+        from_attributes = True
