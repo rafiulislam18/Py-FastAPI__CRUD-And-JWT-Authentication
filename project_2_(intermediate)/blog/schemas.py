f54a18c42d6ca2debc1594c09b55940cebcb6e66
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-19 10:34:55 +0600

    Show User

--- project_2_(intermediate)/blog/schemas.py
+++ project_2_(intermediate)/blog/schemas.py
@@ -17,3 +17,10 @@ class User(BaseModel):
     name: str
     email: str
     password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    
+    class Config():
+        from_attributes = True
