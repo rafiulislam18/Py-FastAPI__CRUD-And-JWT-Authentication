7dfc1af05c77d8045731b88b25eff791985381c7
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-09-01 08:36:40 +0600

    added lesson 3,4,5,6

--- topic_5_query_parameters_and_string_validations/lesson_5.py
+++ topic_5_query_parameters_and_string_validations/lesson_5.py
@@ -1,2 +1,28 @@
-# Lesson Name : 
-# Note : 
+# Lesson Name : Add regular expressions
+# Note : You can define a regular expression pattern that the parameter should match.
+
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+"""
+This specific regular expression pattern checks that the received parameter value:
+
+^: starts with the following characters, doesn't have characters before.
+fixedquery: has the exact value fixedquery.
+$: ends there, doesn't have any more characters after fixedquery.
+"""
--- topic_5_query_parameters_and_string_validations/lesson_6.py
+++ topic_5_query_parameters_and_string_validations/lesson_6.py
@@ -1,2 +1,22 @@
-# Lesson Name : 
-# Note : 
+# Lesson Name : Pydantic v1 regex instead of pattern
+# Note : Before Pydantic version 2 and before FastAPI 0.100.0, the parameter was called regex instead of pattern, but it's now deprecated.
+
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, regex="^fixedquery$")
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+# But know that this is deprecated and it should be updated to use the new parameter pattern.
